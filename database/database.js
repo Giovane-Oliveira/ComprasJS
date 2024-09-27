df29667a199a842f1c0aa1cc177fd104b3e0f33f
Giovane Oliveira <35038647+Giovane-Oliveira@users.noreply.github.com>
2024-09-27 14:23:45 -0300

    Update

--- database/database.js
+++ database/database.js
@@ -1,11 +1,11 @@
 const Sequelize = require("sequelize");
 //azure db admin daruma2024
-const connection = new Sequelize("compras", "admin", "daruma2024", {
+const connection = new Sequelize("compras", "root", "9138", {
   host: "127.0.0.1",
   dialect: "mysql",
   timezone: "-03:00",
   charset: "utf8mb4",
-  collate: "utf8mb4_unicode_ci",
+  collate: "utf8mb4_unicode_ci"
 });
 
 module.exports = connection;
